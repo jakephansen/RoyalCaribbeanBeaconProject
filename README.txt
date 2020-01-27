2af6d5449504a5f7ed19f0adf53518a8f6a5b633
Jacob Hansen <jacobhansen@Jacobs-iMac-Pro.lan1>
2020-01-26 21:33:35 -0600

    Created Database Tables, improved README.txt, connected Raspberry Pi

--- README.txt
+++ README.txt
@@ -29,3 +29,27 @@ Part 4: Populate Database
 
 Part 5: Run Analysis
 - Using the populated Data, Creates Metrics
+
+
+
+
+Dependencies:
+
+pip install psycopg2
+pip install configparser
+create Database, host it, connect in in database.ini file in Database folder
+
+
+
+
+Citations: 
+@article{mohammadi2017semi,
+author={M. Mohammadi and A. Al-Fuqaha and M. Guizani and J. S. Oh},
+journal={IEEE Internet of Things Journal},
+title={{Semi-supervised Deep Reinforcement Learning in Support of IoT and Smart City Services}},
+year={2017},
+pages={1-12},
+publisher={IEEE},
+doi={10.1109/JIOT.2017.2712560},
+ISSN={2327-4662},
+}
--- Scripts/script.py
+++ Scripts/script.py
@@ -1,1 +1,44 @@
 #Authored by Jake Hansen, Jan 2020 in preparation of intervew with Royal Caribbean
+
+class Wristband:
+    def __init__(self, ID, Age, Latitude, Longitude, nearestBeacon):
+        self.ID = ID
+        self.Age = Age
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+        self.nearestBeacon = nearestBeacon
+
+class Vacationer:
+    def __init__(self, ID, Name, Age, Height, Family, Role):
+        self.ID = ID
+        self.Name = Name
+        self.Age = Age
+        self.Height = Height
+        self.Family = Family
+        self.Role = Role
+
+class Beacon:
+    def __init__(self, ID, Latitude, Longitude, Height, Boat, Island, Section, Number, Name):
+        self.ID = ID
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+        self.Height = Height
+        self.Boat = Boat
+        self.Island = Island
+        self.Section = Section
+        self.Number = Number
+        self.Name = Name
+
+class Movement:
+    def __init__(self,PreviousBeacon,CurrentBeacon,Wristband,Time):
+        self.PreviousBeacon = PreviousBeacon
+        self.CurrentBeacon = CurrentBeacon
+        self.Wristband = Wristband
+        self.Time = Time
+
+
+
+
+
+
+print("Hello")
